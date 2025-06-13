227991c7098b910906356ef9f9c81743b2f0c5d6
Zimam <74454732+zimma-web@users.noreply.github.com>
2025-06-13 19:10:12 +0700

    Add zeeflix/.next/static/chunks/src_app_loading_tsx_8450df25._.js 1749816610781

---
+++ zeeflix/.next/static/chunks/src_app_loading_tsx_8450df25._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ef6e3cf8._.js",
+  "static/chunks/src_70872d49._.js"
+],
+    source: "dynamic"
+});
